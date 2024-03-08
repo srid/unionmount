f6bed70641af349647f8375833dc1a02c02e0c12
Sridhar Ratnakumar <srid@srid.ca>
2024-03-08 08:51:31 -0500

    first attempt

--- test/System/UnionMountSpec.hs
+++ test/System/UnionMountSpec.hs
@@ -1,12 +1,45 @@
 module System.UnionMountSpec where
 
-import System.UnionMount
+import Control.Monad.Logger.Extras (logToStderr, runLoggerLoggingT)
+import System.FilePath ((</>))
+import System.FilePattern (FilePattern)
+import System.UnionMount qualified as UM
 import Test.Hspec
+import UnliftIO.Async (race_)
+import UnliftIO.Concurrent (threadDelay)
+import UnliftIO.STM (writeTMVar)
+import UnliftIO.Temporary (withSystemTempDirectory)
 
 -- TODO: Write tests: https://github.com/srid/unionmount/issues/5
 spec :: Spec
 spec = do
-  describe "basics" $ do
-    it "dummy" $ do
-      head (one ()) `shouldBe` ()
-      New `shouldBe` New
+  describe "unionmount" $ do
+    it "single file update works" $ do
+      -- Create a temporary directory, add a file to it, call `mount`, make an update to that file, and check that it is updated in memory.
+      withSystemTempDirectory "unionmount" $ \tempDir -> do
+        writeFile (tempDir </> "file1") "hello"
+        model <- newEmptyTMVarIO
+        flip runLoggerLoggingT logToStderr $ do
+          (model0, update) <- UM.mount tempDir allFiles ignoreNone mempty (const accumulateActions)
+          atomically $ writeTMVar model model0
+          race_
+            (update $ atomically . writeTMVar model)
+            ( do
+                threadDelay 1000000 -- Wait for the initial model to be loaded.
+                writeFile (tempDir </> "file1") "hello, again"
+                threadDelay 1000000 -- Wait for fsnotify to handle events
+            )
+        finalModel <- atomically $ readTMVar model
+        finalModel
+          `shouldBe` [ ("file1", UM.Refresh UM.Existing ()),
+                       ("file1", UM.Refresh UM.Update ())
+                     ]
+
+allFiles :: [((), FilePattern)]
+allFiles = [((), "*")]
+
+ignoreNone :: [a]
+ignoreNone = []
+
+accumulateActions :: (Applicative f, Show a, Show b) => a -> b -> f ([(a, b)] -> [(a, b)])
+accumulateActions fp act = pure $ reverse . (:) (fp, act)
